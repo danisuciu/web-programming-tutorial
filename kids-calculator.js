08a0697bd688ce2699063bab0b58f1860e34a33d
Daniel Suciu <suciudani@gmail.com>
2018-03-26 20:34:20 +0300

    improve using while

--- kids-calculator.js
+++ kids-calculator.js
@@ -12,16 +12,16 @@ function addNumbers(a, b) {
     var suma = a; // numarul cel mai mare
     var degete = b; // numarul cel mai mic
 
-    if (a < b);
+    if (a < b);    {
         suma = b;
         degete = a;
+    }
 
-
-    for (; degete > 0;) {
+    while (degete > 0) {
         degete--; // luam un deget de la b
         suma++; // punem un deget la a
         console.info(suma + ' - au mai ramas ' + degete + ' degete');
     }
     console.info('suma este: ' +suma);
 }
-var count = addNumbers(2, 6);
+var count = addNumbers(6, 16);
